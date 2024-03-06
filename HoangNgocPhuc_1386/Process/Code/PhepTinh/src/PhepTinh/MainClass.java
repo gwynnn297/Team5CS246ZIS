85167c9ea9047122cd1659ce2c4ca3a136ed2ada
gwynnn297 <phuchoang29072000@gmail.com>
2024-03-06 17:08:30 +0700

    source code phép tính

---
+++ HoangNgocPhuc_1386/Process/Code/PhepTinh/src/PhepTinh/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PhepTinh;
+
+/**
+ *
+ * @author tt
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        // Khai bao doi tuong
+        SimpleCalculator slculator = new SimpleCalculator("Simple Calculator");
+    }
+}
