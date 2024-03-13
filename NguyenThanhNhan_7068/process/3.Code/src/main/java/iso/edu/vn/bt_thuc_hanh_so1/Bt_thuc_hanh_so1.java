8aaa52913f1cf45d71a459ce105bc8d72ab319fa
NguyenThanhNhan-7068 <162886633+NguyenThanhNhan-7068@users.noreply.github.com>
2024-03-13 19:43:31 +0700

    update code,sửa lỗi
    sửa lỗi định dạng file và cập nhật code.

---
+++ NguyenThanhNhan_7068/process/3.Code/src/main/java/iso/edu/vn/bt_thuc_hanh_so1/Bt_thuc_hanh_so1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iso.edu.vn.bt_thuc_hanh_so1;
+
+/**
+ *
+ * @author MSI
+ */
+public class Bt_thuc_hanh_so1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
